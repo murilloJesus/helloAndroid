91bfe6f702e87587256bb97904c5f046b665912f
= <=>
2025-08-09 23:41:09 -0300

    helloAndroid

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+  const counter = ref(0);
+
+  function add(){
+    counter.value++
+  }
+</script>
+
+<template>
+  <h1>Hello Android</h1>
+  <h3>Counter: {{ counter }}</h3>
+  <button @click="add">Add</button>
+</template>
+
+<style scoped>
+  button {
+    color: blue;
+    width: 100%;
+  }
+</style>
